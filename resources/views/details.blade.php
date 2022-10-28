9e585136e1c6ffaf826043b7635365f8826ce770
dwizz11 <95602483+dwizz11@users.noreply.github.com>
2022-10-28 16:59:46 +0700

    update height of details product image

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -8,7 +8,7 @@
       {{-- Category --}}
       <div class="container m-auto bg-white d-flex position-absolute top-50 start-50 translate-middle w-50" style="border-radius: 20px; box-shadow: 0px 0px 20px 10px rgba(0, 0, 0, 0.2); text-align: center; padding: 20px !important">
 
-        <div class="container w-50">
+        <div class="container w-50 h-25" style="height: 13em !important; overflow: hidden;">
           <img src="{{ asset('storage/' . $product_item->productphoto) }}" class="card-img-top" alt="{{ asset('storage/' .'img2.jpg') }}">
         </div>
         
@@ -20,7 +20,7 @@
           <form action="/home/buy/{{ $product_item->id }}" method="post">
             @csrf
             <input type="hidden" name="productname" value="{{ $product_item->productname }}">
-            <input type="number" class="form-control mb-2" id="exampleFormControlInput1" placeholder="Quantity" name="quantity" min=0 >
+            <input type="number" class="form-control mb-2" id="exampleFormControlInput1" placeholder="Quantity" name="quantity" min=0>
 
             @error('quantity')
             <div class="error" style="color: red">
