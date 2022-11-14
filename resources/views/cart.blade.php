d4a7231b1b1a4ce95bb485fe726467cd4cb4e82b
dwizz11 <95602483+dwizz11@users.noreply.github.com>
2022-11-14 11:24:26 +0700

    perubahan kecil

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -6,6 +6,8 @@
 <h3 style="text-align: center;" class="mt-4">My Cart</h3>
 <div class="container mt-2 p-5 cart w-50" style="max-height: 40rem !important; overflow-y: scroll; overflow-x: hidden">
 
+  asdasdasdasda
+  halo felix tio
   @php
       $totalprice = 0;
   @endphp
