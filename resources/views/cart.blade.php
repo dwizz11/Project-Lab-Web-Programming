7b9a17e57b67aac0292a8574d1ff32d96f4f2526
Feltoo <felix.tio@binus.ac.id>
2022-11-14 11:12:54 +0700

    perubahan

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -5,9 +5,7 @@
 
 <h3 style="text-align: center;" class="mt-4">My Cart</h3>
 <div class="container mt-2 p-5 cart w-50" style="max-height: 40rem !important; overflow-y: scroll; overflow-x: hidden">
-   haloasdasd
-   blablablbla
-   asdasdasdasdasdasd
+
   @php
       $totalprice = 0;
   @endphp
@@ -18,15 +16,15 @@
         <div class="container w-50 rounded-3 overflow-hidden" style="height: 10em;">
           <img src="{{ asset('storage/' . $item->product->productphoto) }}" class="card-img-top" alt="{{ asset('storage/' .'img/img2.jpg') }}">
         </div>
-        
+
         <div class="container ms-3">
           <p style="margin: 0;text-align: start;font-weight: 700">{{ $item->product->productname}}</p>
           <p style="margin: 0;text-align: start">Quantity : {{ $item->quantity}}</p>
           <p style="margin: 0;text-align: start">Total Price :  <b>IDR {{ number_format(($item->product->price*$item->quantity),2,",",".")}}</b></p>
-          
-      
-            
-         
+
+
+
+
             <form action="/cart/delete/{{ $usercart->id }}/{{ $item->product_id }}" method="post">
               @csrf
               @method('DELETE')
@@ -36,20 +34,20 @@
 
         </div>
 
-      
-      
-        
-     
-      
+
+
+
+
+
       </div>
 
       @php
           $totalprice+=$item->quantity*$item->product->price
       @endphp
       @endforeach
-     
 
-      
+
+
 
     {{-- Category --}}
 </div>
@@ -60,14 +58,14 @@
   <div class="container d-flex align-items-center justify-content-center">
     <p style="font-weight: normal; color: white;margin: 0" class="me-2">Total Price :</p>
     <p style="margin: 0;text-align: start;font-weight: 500;color: white" class="me-3">  IDR {{ number_format($totalprice,2,",",".")  }}</p>
- 
+
     <form action="/cart/purchase" method="post">
       @csrf
        <button type="submit" class="btn btn-secondary ">Purchase</button>
      </form>
   </div>
-  
- 
+
+
 </footer>
 <!-- Footer -->
 
