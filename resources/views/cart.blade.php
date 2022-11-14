13a8e7bc65c9056c75e8d18131049dc545df70c4
Feltoo <felix.tio@binus.ac.id>
2022-11-14 11:38:28 +0700

    asda

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -6,8 +6,7 @@
 <h3 style="text-align: center;" class="mt-4">My Cart</h3>
 <div class="container mt-2 p-5 cart w-50" style="max-height: 40rem !important; overflow-y: scroll; overflow-x: hidden">
 
-  asdasdasdasda
-  halo felix tio
+  asdasdasdasd
   haihai
   @php
       $totalprice = 0;
